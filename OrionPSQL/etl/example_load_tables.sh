c249460e02ae5ba346829142510fc1b4accef525
Arun Kumar <arunkk09@gmail.com>
2017-04-23 10:25:11 -0700

    fixed bug in etl example table names

--- OrionPSQL/etl/example_load_tables.sh
+++ OrionPSQL/etl/example_load_tables.sh
@@ -1,5 +1,5 @@
 echo "Loading S"
-cat s | perl psql_rep_S.pl | psql -c "COPY s FROM STDIN DELIMITER ' '";
+cat s | perl psql_rep_S.pl | psql -c "COPY news FROM STDIN DELIMITER ' '";
 
 echo "Loading R"
-cat r | perl psql_rep_R.pl | psql -c "COPY r FROM STDIN DELIMITER ' '";
+cat r | perl psql_rep_R.pl | psql -c "COPY newr FROM STDIN DELIMITER ' '";
